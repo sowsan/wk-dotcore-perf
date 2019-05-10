df1cb724451c6b1830cf7a479d86e3efc7e8d222
Sowmyan Soman Chullikkattil <sowmyans@microsoft.com>
2019-05-10 11:15:06 -0400

    Add project files.

---
+++ Wk.DotNetCore.Perf/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Wk.DotNetCore.Perf
+{
+    public  class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Starting the work....");
+
+            BenchmarkRunner.Run<BenchMarkTest>();
+          
+            Console.ReadLine();
+        }
+    }
+
+
+    [SimpleJob(launchCount: 10)]
+    [RPlotExporter, RankColumn]
+    public class BenchMarkTest
+    {
+        Suspect row = new Suspect();
+
+        public BenchMarkTest()
+        {
+            row.Set("StringFieldKey", "somelongandbigvalue");
+            row.Set("BoolFieldKey", (bool?)true);
+            row.Set("DecimalFiledKey", (decimal?)12.34m);
+            row.Set("DateFieldKey", (DateTime?)DateTime.UtcNow);
+            row.Set("NullFieldKey", (decimal?)null);           
+        }
+
+        [Benchmark]
+        public void GetOrDefaultSlow()
+        {
+            var t = row.GetOrDefaultSlow<decimal?>("DecimalFiledKey");
+        }
+    }
+}
---
+++ Wk.DotNetCore.Perf/Suspect.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Wk.DotNetCore.Perf
+{
+    public class Suspect
+    {
+        private readonly Dictionary<string, object> _store;
+
+        public Suspect()
+            : this(new Dictionary<string, object>(StringComparer.Ordinal))
+        {
+        }
+
+        public Suspect(Dictionary<string, object> store)
+        {
+            _store = new Dictionary<string, object>(store, StringComparer.Ordinal);
+        }
+        
+        public T GetOrDefault<T>(string propertyName)
+        {
+            if (!_store.ContainsKey(propertyName))
+            {
+                return default(T);
+            }
+
+            return (T)_store[propertyName];
+        }
+               
+        [Obsolete("Will be removed in next version, use GetOrDefault<T> instead")]
+        public T GetOrDefaultSlow<T>(string propertyName)
+        {
+            if (!_store.ContainsKey(propertyName))
+            {
+                return default(T);
+            }
+
+            var propertyValue = _store[propertyName];
+            if (propertyValue == null)
+            {
+                return (T)propertyValue;
+            }
+
+            var propertyValueType = propertyValue.GetType();
+            var targetType = typeof(T);
+            if (propertyValueType == targetType)
+            {
+                return (T)_store[propertyName];
+            }
+
+            if (targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                targetType = Nullable.GetUnderlyingType(targetType);
+            }
+
+            return (T)Convert.ChangeType(propertyValue, targetType);
+        }
+
+        public void Set<T>(string propertyName, T value)
+        {
+            if (_store.ContainsKey(propertyName))
+            {
+                _store[propertyName] = value;
+            }
+            else
+            {
+                _store.Add(propertyName, value);
+            }
+        }
+    }
+}
